9c9a52c01f81b65dc1e37241715f072055c6b7d6
Joel John <110528386+joeljohn9699@users.noreply.github.com>
2022-10-05 19:59:29 +0530

    Add files via upload

---
+++ Assignment1G1B5/src/com/gl/service/SuperDepartment.java
@@ -0,0 +1,21 @@
+package com.gl.service;
+
+public class SuperDepartment {
+	
+	private String departmentName() {
+		return "Super Department";
+	}
+	
+	private String getTodaysWork() {
+		return "No Work as of now";
+	}
+	
+	private String getWorkDeadline() {
+		return "Nil";
+	}
+	
+	protected String isTodayAHoliday() {
+		return "Today is not a Holiday";
+	}
+
+}
